d6bb87b4b710ed1b9f352c25ae7ce337d17d3379
Robert J. Berger <rberger@Psion.local>
2010-06-07 22:45:54 -0700

    Renamed hadoop directory and updated metadata

--- site-cookbooks/hadoop_for_hbase/metadata.rb
+++ site-cookbooks/hadoop_for_hbase/metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "Runa Inc."
+maintainer       "Robert J. Berger Runa Inc."
 maintainer_email "ops@runa.com"
 license          "Apache 2.0"
-description      "Installs/Configures hadoop"
+description      "Installs/Configures hadoop for the matching HBase Recipe using a common Databag for configuration"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
--- site-cookbooks/hbase/metadata.rb
+++ site-cookbooks/hbase/metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "Runa Inc."
+maintainer       "Robert J. Berger Runa Inc."
 maintainer_email "ops@runa.com"
 license          "Apache 2.0"
-description      "Installs/Configures hbase"
+description      "Installs/Configures hbase using a Databag for configuration. Expects Hadoop to be set up with the hadoop_for_hbase recipe"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
